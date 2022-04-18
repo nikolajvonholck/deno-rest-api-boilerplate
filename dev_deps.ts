7998a654e8031ca0e40e827286313a910afa4276
Nikolaj von Holck <mail@nikolajholck.dk>
2022-04-18 17:25:37 +0200

    Make test more easy to read.

--- dev_deps.ts
+++ dev_deps.ts
@@ -3,3 +3,8 @@ export {
   assert,
   assertEquals,
 } from "https://deno.land/std@0.135.0/testing/asserts.ts";
+export {
+  afterEach,
+  describe,
+  it,
+} from "https://deno.land/std@0.135.0/testing/bdd.ts";
--- tests/todos.e2e.test.ts
+++ tests/todos.e2e.test.ts
@@ -1,5 +1,11 @@
 import { app } from "../app.ts";
-import { assertEquals, superoak } from "../dev_deps.ts";
+import {
+  afterEach,
+  assertEquals,
+  describe,
+  it,
+  superoak,
+} from "../dev_deps.ts";
 import { TodoDTO } from "../models/Todo.ts";
 import { todoRepository } from "../repositories/todoRepository.ts";
 import { database } from "../services/database.ts";
@@ -12,96 +18,118 @@ const todoDtoCreateMalformed = { title: 42 };
 const todoDtoUpdate = { title: "New title", isCompleted: true };
 const todoDtoUpdateMalformed = { isCompleted: 42 };
 
-Deno.test("todo router", async (t) => {
-  await t.step(
-    "returns error if attempting to create todo with malformed input",
-    async () => {
+describe("todo router", () => {
+  afterEach(async () => {
+    await database.close();
+  });
+
+  describe("POST todos/", () => {
+    it(
+      "returns error if attempting to create todo with malformed input",
+      async () => {
+        const request = await superoak(app);
+        const response = await request
+          .post(path)
+          .send(todoDtoCreateMalformed)
+          .expect(400);
+        assertError(response.body);
+      },
+    );
+
+    it("can create todo with well-formed input", async () => {
       const request = await superoak(app);
       const response = await request
         .post(path)
-        .send(todoDtoCreateMalformed)
-        .expect(400);
+        .send(todoDtoCreate)
+        .expect(201);
+      const todo = assertOk(response.body as Result<TodoDTO>);
+      const { id, title, isCompleted } = todo;
+      assertEquals(title, todoDtoCreate.title);
+      assertEquals(isCompleted, false);
+
+      // Verify that object has been saved in database.
+      const todoFromDatabase = await todoRepository.findById(id);
+      assertEquals(databaseObjectToDTO(todoFromDatabase), todo);
+    });
+  });
+
+  describe("GET todos/:id", () => {
+    it("cannot read todo if it does not exist", async () => {
+      const uuid = globalThis.crypto.randomUUID();
+
+      const request = await superoak(app);
+      const response = await request
+        .get(`${path}/${uuid}`)
+        .expect(404);
       assertError(response.body);
-      await database.close();
-    },
-  );
+    });
 
-  await t.step("can create todo with well-formed input", async () => {
-    const request = await superoak(app);
-    const response = await request
-      .post(path)
-      .send(todoDtoCreate)
-      .expect(201);
-    const todo = assertOk(response.body as Result<TodoDTO>);
-    const { id, title, isCompleted } = todo;
-    assertEquals(title, todoDtoCreate.title);
-    assertEquals(isCompleted, false);
+    it("can read todo", async () => {
+      const todoFromDatabase = await todoRepository.create(todoDtoCreate);
+      const id = todoFromDatabase.id as string;
 
-    // Verify that object has been saved in database.
-    const todoFromDatabase = await todoRepository.findById(id);
-    assertEquals(databaseObjectToDTO(todoFromDatabase), todo);
+      const request = await superoak(app);
+      const response = await request
+        .get(`${path}/${id}`)
+        .expect(200);
+      const todo = assertOk(response.body as Result<TodoDTO>);
 
-    await database.close();
+      // Verify that object has been read from database.
+      assertEquals(todo, databaseObjectToDTO(todoFromDatabase));
+    });
   });
 
-  await t.step("cannot read todo if it does not exist", async () => {
-    const uuid = globalThis.crypto.randomUUID();
+  describe("GET todos/", () => {
+    it("can read all todos", async () => {
+      const request = await superoak(app);
+      const response = await request
+        .get(path)
+        .expect(200);
+      const todos = assertOk(response.body as Result<TodoDTO[]>);
+      const todosFromDatabase = await todoRepository.findAll();
 
-    const request = await superoak(app);
-    const response = await request
-      .get(`${path}/${uuid}`)
-      .expect(404);
-    assertError(response.body);
-
-    await database.close();
+      // Verify that objects have been read from database.
+      assertEquals(todos, todosFromDatabase.map(databaseObjectToDTO));
+    });
   });
 
-  await t.step("can read todo", async () => {
-    const todoFromDatabase = await todoRepository.create(todoDtoCreate);
-    const id = todoFromDatabase.id as string;
+  describe("PUT todos/:id", () => {
+    it("cannot update todo if it does not exist", async () => {
+      const uuid = globalThis.crypto.randomUUID();
 
-    const request = await superoak(app);
-    const response = await request
-      .get(`${path}/${id}`)
-      .expect(200);
-    const todo = assertOk(response.body as Result<TodoDTO>);
+      const request = await superoak(app);
+      const response = await request
+        .put(`${path}/${uuid}`)
+        .send(todoDtoUpdate)
+        .expect(404);
+      assertError(response.body);
+    });
 
-    // Verify that object has been read from database.
-    assertEquals(todo, databaseObjectToDTO(todoFromDatabase));
+    it(
+      "returns error if attempting to update todo with malformed input",
+      async () => {
+        const todoFromDatabaseBefore = await todoRepository.create(
+          todoDtoCreate,
+        );
+        const id = todoFromDatabaseBefore.id as string;
 
-    await database.close();
-  });
+        const request = await superoak(app);
+        const response = await request
+          .put(`${path}/${id}`)
+          .send(todoDtoUpdateMalformed)
+          .expect(400);
+        assertError(response.body);
 
-  await t.step("can read all todos", async () => {
-    const request = await superoak(app);
-    const response = await request
-      .get(path)
-      .expect(200);
-    const todos = assertOk(response.body as Result<TodoDTO[]>);
-    const todosFromDatabase = await todoRepository.findAll();
+        // Verify that object is unaffected in database.
+        const todoFromDatabaseAfter = await todoRepository.findById(id);
+        assertEquals(
+          databaseObjectToDTO(todoFromDatabaseAfter),
+          databaseObjectToDTO(todoFromDatabaseBefore),
+        );
+      },
+    );
 
-    // Verify that objects have been read from database.
-    assertEquals(todos, todosFromDatabase.map(databaseObjectToDTO));
-
-    await database.close();
-  });
-
-  await t.step("cannot update todo if it does not exist", async () => {
-    const uuid = globalThis.crypto.randomUUID();
-
-    const request = await superoak(app);
-    const response = await request
-      .put(`${path}/${uuid}`)
-      .send(todoDtoUpdate)
-      .expect(404);
-    assertError(response.body);
-
-    await database.close();
-  });
-
-  await t.step(
-    "returns error if attempting to update todo with malformed input",
-    async () => {
+    it("can update todo with well-formed input", async () => {
       const todoFromDatabaseBefore = await todoRepository.create(
         todoDtoCreate,
       );
@@ -110,77 +138,50 @@ Deno.test("todo router", async (t) => {
       const request = await superoak(app);
       const response = await request
         .put(`${path}/${id}`)
-        .send(todoDtoUpdateMalformed)
-        .expect(400);
-      assertError(response.body);
+        .send(todoDtoUpdate)
+        .expect(200);
+      const todo = assertOk(response.body as Result<TodoDTO>);
+      assertEquals(todo.title, todoDtoUpdate.title);
+      assertEquals(todo.isCompleted, todoDtoUpdate.isCompleted);
 
-      // Verify that object is unaffected in database.
+      // Verify that object has been saved in database.
       const todoFromDatabaseAfter = await todoRepository.findById(id);
-      assertEquals(
-        databaseObjectToDTO(todoFromDatabaseAfter),
-        databaseObjectToDTO(todoFromDatabaseBefore),
+      // Ignore property 'updatedAt'.
+      const { updatedAt: _, ...before } = databaseObjectToDTO(
+        todoFromDatabaseBefore,
       );
-
-      await database.close();
-    },
-  );
-
-  await t.step("can update todo with well-formed input", async () => {
-    const todoFromDatabaseBefore = await todoRepository.create(
-      todoDtoCreate,
-    );
-    const id = todoFromDatabaseBefore.id as string;
-
-    const request = await superoak(app);
-    const response = await request
-      .put(`${path}/${id}`)
-      .send(todoDtoUpdate)
-      .expect(200);
-    const todo = assertOk(response.body as Result<TodoDTO>);
-    assertEquals(todo.title, todoDtoUpdate.title);
-    assertEquals(todo.isCompleted, todoDtoUpdate.isCompleted);
-
-    // Verify that object has been saved in database.
-    const todoFromDatabaseAfter = await todoRepository.findById(id);
-    // Ignore property 'updatedAt'.
-    const { updatedAt: _, ...before } = databaseObjectToDTO(
-      todoFromDatabaseBefore,
-    );
-    const { updatedAt: __, ...after } = databaseObjectToDTO(
-      todoFromDatabaseAfter,
-    );
-    assertEquals({ ...before, ...todoDtoUpdate }, after);
-
-    await database.close();
+      const { updatedAt: __, ...after } = databaseObjectToDTO(
+        todoFromDatabaseAfter,
+      );
+      assertEquals({ ...before, ...todoDtoUpdate }, after);
+    });
   });
 
-  await t.step("cannot delete todo if it does not exist", async () => {
-    const uuid = globalThis.crypto.randomUUID();
+  describe("DELETE todos/:id", () => {
+    it("cannot delete todo if it does not exist", async () => {
+      const uuid = globalThis.crypto.randomUUID();
 
-    const request = await superoak(app);
-    const response = await request
-      .delete(`${path}/${uuid}`)
-      .expect(404);
-    assertError(response.body);
+      const request = await superoak(app);
+      const response = await request
+        .delete(`${path}/${uuid}`)
+        .expect(404);
+      assertError(response.body);
+    });
 
-    await database.close();
-  });
+    it("can delete todo", async () => {
+      const todoFromDatabase = await todoRepository.create(todoDtoCreate);
+      const id = todoFromDatabase.id as string;
 
-  await t.step("can delete todo", async () => {
-    const todoFromDatabase = await todoRepository.create(todoDtoCreate);
-    const id = todoFromDatabase.id as string;
+      const request = await superoak(app);
+      const response = await request
+        .delete(`${path}/${id}`)
+        .expect(200);
+      const v = assertOk(response.body as Result<undefined>);
+      assertEquals(v, undefined);
 
-    const request = await superoak(app);
-    const response = await request
-      .delete(`${path}/${id}`)
-      .expect(200);
-    const v = assertOk(response.body as Result<undefined>);
-    assertEquals(v, undefined);
-
-    // Verify that object has been delete from database.
-    const todoFromDatabaseAfter = await todoRepository.findById(id);
-    assertEquals(todoFromDatabaseAfter, undefined);
-
-    await database.close();
+      // Verify that object has been delete from database.
+      const todoFromDatabaseAfter = await todoRepository.findById(id);
+      assertEquals(todoFromDatabaseAfter, undefined);
+    });
   });
 });
