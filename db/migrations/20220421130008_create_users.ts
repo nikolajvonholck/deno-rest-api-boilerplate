0bc6ca5e50cc6760d0938171ddc93bad70db7d76
Nikolaj von Holck <mail@nikolajholck.dk>
2022-04-21 15:02:06 +0200

    Create 20220421130008_create_users.ts

---
+++ db/migrations/20220421130008_create_users.ts
@@ -0,0 +1,24 @@
+import {
+  AbstractMigration,
+  ClientPostgreSQL,
+} from "https://deno.land/x/nessie@2.0.5/mod.ts";
+
+export default class extends AbstractMigration<ClientPostgreSQL> {
+  /** Runs on migrate */
+  async up(): Promise<void> {
+    await this.client.queryArray(`CREATE TABLE users
+        (
+            id uuid NOT NULL,
+            email character varying NOT NULL,
+            password_hash character varying NOT NULL,
+            created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            CONSTRAINT id PRIMARY KEY (id)
+        )`);
+  }
+
+  /** Runs on rollback */
+  async down(): Promise<void> {
+    await this.client.queryArray(`DROP TABLE users`);
+  }
+}
