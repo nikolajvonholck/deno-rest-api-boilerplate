a0f527097c62f2c069a1ec4643e62c548e6dce81
Nikolaj von Holck <mail@nikolajholck.dk>
2022-05-01 15:58:19 +0200

    Alter migrations.
    This should never be done on a production codebase, but it's fine for this boilerplate repo.

--- db/migrations/20220413130008_create_users.ts
+++ db/migrations/20220413130008_create_users.ts
@@ -7,14 +7,14 @@ export default class extends AbstractMigration<ClientPostgreSQL> {
   /** Runs on migrate */
   async up(): Promise<void> {
     await this.client.queryArray(`CREATE TABLE users
-        (
-            id uuid NOT NULL,
-            email character varying NOT NULL,
-            password_hash character varying NOT NULL,
-            created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            CONSTRAINT users_id PRIMARY KEY (id)
-        )`);
+      (
+        id uuid NOT NULL,
+        email character varying NOT NULL,
+        password_hash character varying NOT NULL,
+        created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        CONSTRAINT fk_users_id PRIMARY KEY (id)
+      )`);
   }
 
   /** Runs on rollback */
--- db/migrations/20220414213955_create_todos.ts
+++ db/migrations/20220414213955_create_todos.ts
@@ -7,14 +7,17 @@ export default class extends AbstractMigration<ClientPostgreSQL> {
   /** Runs on migrate */
   async up(): Promise<void> {
     await this.client.queryArray(`CREATE TABLE todos
-        (
-            id uuid NOT NULL,
-            title character varying NOT NULL,
-            is_completed boolean NOT NULL DEFAULT false,
-            created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            CONSTRAINT todos_id PRIMARY KEY (id)
-        )`);
+      (
+        id uuid NOT NULL,
+        title character varying NOT NULL,
+        is_completed boolean NOT NULL,
+        created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        user_id uuid NOT NULL,
+        CONSTRAINT pk_todos_id PRIMARY KEY (id),
+        CONSTRAINT fk_todos_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+      )`);
   }
 
   /** Runs on rollback */
