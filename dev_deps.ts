92c8d9b76537a4749a9e4645d038c218cfd64981
Nikolaj von Holck <mail@nikolajholck.dk>
2022-05-01 19:41:44 +0200

    Update dev_deps

--- dev_deps.ts
+++ dev_deps.ts
@@ -2,9 +2,9 @@ export { superoak } from "https://deno.land/x/superoak@4.7.0/mod.ts";
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.135.0/testing/asserts.ts";
+} from "https://deno.land/std@0.137.0/testing/asserts.ts";
 export {
   afterEach,
   describe,
   it,
-} from "https://deno.land/std@0.135.0/testing/bdd.ts";
+} from "https://deno.land/std@0.137.0/testing/bdd.ts";
